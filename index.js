cd4ad0f7ca8dcbf7718519aa8fc6149c9fc49cc6
Hannes <Hannes.Holzwarth@me.com>
2016-06-15 22:29:09 +0200

    The user middleware is not included by default anymore

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
 'use strict';
 
-const Alfred = require('./lib/index');
-const User = require('./lib/user');
-
-module.exports = class extends Alfred {
-
-	constructor(options) {
-		super(options);
-		
-		this.use(this.user = User);
-	}
-}
-
-module.exports.Base = Alfred;
-module.exports.User = User;
+module.exports = require('./lib/index');
+module.exports.User = require('./lib/user');
